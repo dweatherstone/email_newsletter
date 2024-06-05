65652ba4a58fc86e6eb8c16d62e260d5ebd6410e
David Weatherstone <davidweatherstone@gmail.com>
2024-06-05 09:28:53 +0100

    Chapter 3 Part 1: added a health_check endpoint and wired up integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use actix_email_newsletter::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("http://127.0.0.1:8000").expect("Failed to bind a listener");
+    run(listener)?.await
 }

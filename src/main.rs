2a5816f484c674332ec4681811e8ff7bb4084816
David Weatherstone <davidweatherstone@gmail.com>
2024-06-07 09:52:43 +0100

    Adding telemetry logging

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use std::net::TcpListener;
 
-use actix_email_newsletter::{configuration::get_configuration, startup::run};
+use actix_email_newsletter::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber(
+        "email_newsletter".to_string(),
+        "info".to_string(),
+        std::io::stdout,
+    );
+    init_subscriber(subscriber);
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
